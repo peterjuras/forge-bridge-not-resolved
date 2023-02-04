0a4fa9e5c74afad22400720fa6ea6fa82d8e1c78
Peter Juras <me@peterjuras.com>
2023-02-04 23:24:45 +0100

    Add @forge/bridge example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,13 @@
+import { view } from "@forge/bridge"
+import { useEffect } from "react"
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
+  useEffect(() => {
+    view.getContext().then((context) => {
+      console.log(context)
+    })
+  } ,[])
+
   return null
 }
